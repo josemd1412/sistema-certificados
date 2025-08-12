f0490f23611231eb7fbae276af20851b62531a33
josemd1412 <jemd2203@gmail.com>
2025-08-12 15:53:04 -0500

    UPD: cargando las nuevas entidades actualizadas, los archivos services actualizados

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+  .setTitle('Certificados API')
+  .setDescription('API para gestionar certificados')
+  .setVersion('2.0')
+  .addBearerAuth()
+  .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document, {
+    customSiteTitle: "API certificados",
+    swaggerOptions:{
+      persistAuthorization: true,
+      docExpansion: 'list',
+    },
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
