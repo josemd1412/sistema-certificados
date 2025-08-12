29a69bbf92bd704c3e9d849976ccd8a4b32eed92
josemd1412 <jemd2203@gmail.com>
2025-08-12 15:57:18 -0500

    UPD: actualizando para el uso de swagger

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,33 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { CertificatosModulo } from './certificados/certificados.module';
+import { CertificadosModule } from './certificados/certificados.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
+
 @Module({
-  imports: [TypeOrmModule.forRoot(), CertificatosModulo],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get<string>('DB_USERNAME'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_DATABASE'),
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        synchronize: configService.get<boolean>('DB_SYNCHRONIZE'),
+        logging: configService.get<boolean>('DB_LOGGING'),
+        extra: {
+          ssl: configService.get("NODE_ENV") === 'production' ? {rejectUnauthorized: false} : false,
+      }
+    })
+    }), 
+    CertificadosModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
